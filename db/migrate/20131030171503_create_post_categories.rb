bd9a7dc3c95a87212345d7fac6e18f7c10934777
Waine Tam <wainetam@gmail.com>
2013-10-31 22:09:40 -0400

    initial commit

---
+++ db/migrate/20131030171503_create_post_categories.rb
@@ -0,0 +1,10 @@
+class CreatePostCategories < ActiveRecord::Migration
+  def change
+    create_table :post_categories do |t|
+      t.belongs_to :post
+      t.belongs_to :category
+
+      t.timestamps
+    end
+  end
+end
